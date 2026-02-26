3fb1537e5fa09c3e2f0577e620aa484e1d6716ba
Aya <ayahame99@gmail.com>
2026-02-26 19:12:30 +0200

    intializing Screens

---
+++ lib/screens/cart_empty_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CartEmptyScreen extends StatelessWidget {
+   static const  String routeName = '/CartEmptyScreen';
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Cart Empty Screen"),
+    );
+  }
+}
---
+++ lib/screens/cart_full_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CartFullScreen extends StatelessWidget {
+  static const  String routeName = '/cartFullScreen';
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Cart Full Screen"),
+    );
+  }
+}
